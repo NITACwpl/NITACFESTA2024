38e7e00cf9c35caf42050e6dc3a86a70ca225cb8
belbosco <e2203@s.akashi.ac.jp>
2024-10-06 01:47:04 +0900

    トップページ経過途中１

--- top.js
+++ top.js
@@ -1,2 +1,12 @@
-
+/* toppage-timetable */
+$(function() {
+    $(".poster1").click(function(){
+        location.href='nofilled'
+        //nofilledには遷移先のurl
+    });
+    $(".poster2").click(function(){
+        location.href='nofilled'
+        //nofilledには遷移先のurl
+    });
+});
   
